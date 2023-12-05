7775b7d4de29cd3f5bc0c6f778d17ba15fa45617
noooaah <abdoelginehab435@gmail.com>
2023-12-05 03:30:46 +0200

    import modules

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for x in my_string:
-        my_string.remove("c","C")
-        return my_string
+    new_string = ""
+    for c in my_string:
+        if c not in "Cc":
+            new_string += c
+    return new_string
